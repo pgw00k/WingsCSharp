3e002575c19480aa0cc90c97ee06147595e3e050
Administrator <Administrator@tencent.com>
2022-05-19 10:44:31 +0800

    [Init]Project

---
+++ WingsCSharp/WingsCSharp.Test.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WingsCSharp.Test.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
