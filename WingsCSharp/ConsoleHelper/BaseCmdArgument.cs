fcbb7bbafdbf5b94113bcc7394ab8318ee7820e6
Wings <pgw00k@163.com>
2023-02-02 16:56:54 +0800

    [ADD]命令行使用的常用工具 [Update]使用VS2022来进行管理

---
+++ WingsCSharp/ConsoleHelper/BaseCmdArgument.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenOcean.Common
+{
+    public class BaseCmdArgument
+    {
+        public delegate int OptionAction(BaseCmdArgument self, string arg, int argIndex);
+
+        public string[] Arguments;
+
+        public Dictionary<string, OptionAction> OptionActions = new Dictionary<string, OptionAction>();
+
+        public BaseCmdArgument(string[] args)
+        {
+            Arguments = args;
+        }
+
+        public virtual void ProcessArgs()
+        {
+            int count = Arguments.Length;
+            for (int i = 0; i < count; i++)
+            {
+                var arg = Arguments[i];
+                if(arg.StartsWith("-"))
+                {
+                    arg = arg.ToLower();
+                    if(OptionActions.TryGetValue(arg,out OptionAction act))
+                    {
+                        i+=act.Invoke(this, arg, i);
+                    }else
+                    {
+                        Console.WriteLine($"Argument {arg} is not defined.");
+                    }
+                }
+            }
+        }
+    }
+}
