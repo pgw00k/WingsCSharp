291f8dbcc4953a4801a7ee93db17bf04b9a6773f
Wings <pgw00k@163.com>
2023-02-05 23:44:50 +0800

    [Update]添加命令行输出控制

---
+++ WingsCSharp/Common/LoggerConsoleWriter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenOcean.Common
+{
+    public class LoggerConsoleWriter : StreamWriter
+    {
+        public LoggerConsoleWriter(Stream stream) : base(stream)
+        {
+            AutoFlush = true;
+            Console.SetOut(this);
+        }
+    }
+}
