6ee5521c8ec7c3f0f62a62448e235e2d8733b922
Wings <pgw00k@163.com>
2023-08-03 10:28:58 +0800

    [Add]添加请求序列化

---
+++ WingsCSharp/NetExtension/RequestExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Net
+{
+    public static class RequestExtension
+    {
+        public static string ToStringEx(this HttpListenerRequest request)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (string headerKey in request.Headers.AllKeys)
+            {
+                sb.AppendLine($"{headerKey}:{request.Headers[headerKey]}");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
